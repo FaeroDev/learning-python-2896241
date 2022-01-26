b8d1a68aec11e7b7fe72b5f1fc680096483c0dd8
Matthew Pawlak <mpawlak@flexion.us>
2022-01-26 11:08:47 -0500

    updates

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,10 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    print("Hello World")
+    name = input("what is your name? ")
+    print("nice to meet you", name)
 
+if __name__ == "__main__":
+    main()
